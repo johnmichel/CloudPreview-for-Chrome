0e4fdf13009877f5da30137f0eeb9199fbc74ed2
Tobias Tom <t.tom@succont.de>
2010-06-29 19:52:28 +0200

    initial commit

---
+++ Main.js
@@ -0,0 +1,28 @@
+window.addEventListener( 'DOMContentLoaded', function() {
+	var extensionPos = document.title.lastIndexOf('.')
+	if ( !extensionPos ) {
+		return;
+	}
+	var extension = document.title.substr( extensionPos + 1 )
+	switch ( extension ) {
+		case 'mov':
+			var videoPlayer = document.createElement( 'video' )
+			videoPlayer.controls = true
+			videoPlayer.src = document.querySelector( '.download a').href
+
+			videoPlayer.addEventListener( 'loadedmetadata', function() {
+				document.getElementById('wrapper').style.width = ( this.videoWidth + 74 ) + 'px'
+			}, false )
+			
+			var container = document.querySelector( '#container .item' )
+			container.appendChild( videoPlayer )
+
+			document.querySelector('h1').style.marginBottom = '20px'
+			break;
+		default:
+			// do nothing
+			break;
+	}
+}, false );
+
+console.log( 'CloudPreview enabled' )
---
+++ README.markdown
@@ -0,0 +1,5 @@
+# CloudPreview
+
+This Safari 5 extension will display some more inline previews on the http://cl.ly website.
+
+A little bit more information is available on it's website: [tobiastom.github.com/CloudPreview.safariextension](http://tobiastom.github.com/CloudPreview.safariextension).
